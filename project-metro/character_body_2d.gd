78bf0bc88f679a7ba68cff5320d2c442fa38dbda
pizzacrisp <antoxamtl123@gmail.com.com>
2025-04-16 17:07:25 -0700

    Clean up

--- project-metro/character_body_2d.gd
+++ project-metro/character_body_2d.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var anim_sprite = $AnimatedSprite2D
+
+func _ready():
+	# Set up initial animation state
+	anim_sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		velocity.y += gravity * delta
+		anim_sprite.play("jump")
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		anim_sprite.play("jump")
+	
+	# Handle attack animation
+	if Input.is_action_just_pressed("attack"):  # You'll need to define this input action
+		anim_sprite.play("attack")
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		# Flip sprite based on direction
+		anim_sprite.flip_h = direction < 0
+		if is_on_floor():
+			anim_sprite.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if is_on_floor():
+			anim_sprite.play("idle")
+	
 	move_and_slide()
