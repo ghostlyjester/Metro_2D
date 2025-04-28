4952077ae6435be53d3d126036642483ca5ee6bf
7Bssk7 <arhip.fin3@gmail.com>
2025-04-27 19:39:18 -0700

    Added HealthBar

--- project-metro/src/canvas_layer.gd
+++ project-metro/src/canvas_layer.gd
@@ -1,13 +1,15 @@
 extends CanvasLayer
 
 @onready var timer: Timer = $Timer
+@onready var health_bar: ProgressBar = $HealthBar
 
 var health_val = 100
 
 
 func _ready():
-	$Label.text= "Health: "
+	$Label.text= "Health: 100/ "
 	$health.text = str(health_val)
+	health_bar.value = health_val
 
 func _input(event):
 	
@@ -15,11 +17,15 @@ func _input(event):
 		
 		health_val -= 20
 		
+		health_bar.value = health_val
+		
 		$health.text = str(health_val)
 	
 	if(health_val < 1):
 		
 		$health.text = "..."
+		
+		health_bar.value = health_val
 		
 		timer.start()
 
--- project-metro/src/character_body_2d.gd
+++ project-metro/src/character_body_2d.gd
@@ -9,10 +9,12 @@ const JUMP_VELOCITY = -600.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var is_attacking = false;
+
 @onready var enemy: Node2D = $"../enemy"
 @onready var health_label: Label = $"../CanvasLayer/health"
 @onready var health: CanvasLayer = $"../CanvasLayer"
 @onready var anim_sprite = $AnimatedSprite2D
+@onready var health_bar: ProgressBar = $"../CanvasLayer/HealthBar"
 
 func _ready():
 	# Set up initial animation state
@@ -56,42 +58,19 @@ func _physics_process(delta: float) -> void:
 			if Input.is_action_just_pressed("jump") and is_on_floor():
 				velocity.y = JUMP_VELOCITY
 				anim_sprite.play("idle")
-				
-			
-	
-	#if direction:
-		#velocity.x = direction * SPEED
-		# Flip sprite based on direction
-		#anim_sprite.flip_h = direction < 0
-		#if is_on_floor():
-			#anim_sprite.play("run")
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#if is_on_floor():
-			#anim_sprite.play("idle")
-	# Handle jump.
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-		#anim_sprite.play("idle")
-	
-	# Handle attack animation
-	#if Input.is_action_just_pressed("attack_user"):  # You'll need to define this input action
-	#	anim_sprite.play("attack")
-	
-	# Get the input direction and handle the movement/deceleration.
 	
 	move_and_slide()
 	
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	print("Collided")
 	if area.is_in_group("enemy") and !is_attacking and !enemy.is_dead:
 		print("Health - 10")
 		health.health_val = health.health_val - 10
+		health_bar.value = health.health_val
 		health_label.text = str(health.health_val)
 		
-	elif area.is_in_group("enemy") and is_attacking:
-		print("Enemy Health - 90")
-		enemy.health_val = enemy.health_val - 90
+	elif area.is_in_group("enemy") and is_attacking and !enemy.is_dead:
+		print("Enemy Health - 30")
+		enemy.health_val = enemy.health_val - 30
 	
